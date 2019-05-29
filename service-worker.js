0ed68f602e155b721080874e041c6ea1a6ea38e7
Yudhanira Ela <ela.yudhanira@scania.com>
2019-05-29 15:06:03 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-cui/precache-manifest.7f0bc06864e42011a157b3daa4c30d56.js"
+  "/corporate-ui-react/precache-manifest.09ebdd780d5f63d483f3edb91fe73b37.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-cui/index.html", {
+workbox.routing.registerNavigationRoute("/corporate-ui-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
