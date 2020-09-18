e47d3e6f42cf219132d24797da14560889fb8361
Marcus Mena <marcus.mena.pacheco@scania.com>
2020-09-18 13:36:31 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/corporate-ui-react/precache-manifest.682449702b534ca76404b1fbc071e32a.js"
+  "/corporate-ui-react/precache-manifest.02a8efd205703e158a3c51e1d56aecce.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/corporate-ui-react/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
